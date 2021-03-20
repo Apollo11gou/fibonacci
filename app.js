adf13af97dbb3fad997a602bb73844dff1459444
Apollo11gou <next.soharuna@gmail.com>
2021-03-20 14:50:32 +0900

    フィボナッチ数列を作成する関数をトリボナッチ関数を作成する関数に変更

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 "use strict";
 const memo = new Map();
-// 0 番目と 1 番目の答えは予め定義されているため記述
+// 0 番目 、1 番目と2番目の答えは予め定義されているため記述
 memo.set(0, 0);
-memo.set(1, 1);
-// もし Map が n をキーとした答えを持っていればその値をそのまま関数の値として返します。 そうでない場合は再帰関数を計算して値を求め、それを Map に格納した後、返す
-function fib(n) {
+memo.set(1, 0);
+memo.set(2, 1);
+// トリボナッチ数列を出力する関数
+function trib(n) {
   if (memo.has(n)) {
     return memo.get(n);
   }
-  const value = fib(n - 1) + fib(n - 2);
+  const value = trib(n - 1) + trib(n - 2) + trib(n - 3);
   memo.set(n, value);
   return value;
 }
 
 const length = 40;
 for (let i = 0; i <= length; i++) {
-  console.log(fib(i));
+  console.log(trib(i));
 }
